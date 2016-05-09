1b99362cb2a6b01b2136db4d3e4f2bd5fa01915b
Chelsea Johnson <cheljoh@gmail.com>
2016-05-09 11:02:09 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.5.1'
+gem 'pg', '~> 0.15'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'rails_12factor', group: :production
+gem 'selenium-webdriver'
+
+group :development, :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'pry-rails'
+  gem 'capybara'
+  gem 'launchy'
+  gem 'database_cleaner'
+end
+
+group :test do
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'simplecov', require: false
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+require "simplecov"
+
+module WaitForAjax
+  def wait_for_ajax
+    Capybara.default_max_wait_time = 10
+    Timeout.timeout(Capybara.default_max_wait_time) do
+      loop until finished_all_ajax_requests?
+    end
+  end
+
+  def finished_all_ajax_requests?
+    page.evaluate_script('jQuery.active').zero?
+  end
+end
+
+RSpec.configure do |config|
+  config.include WaitForAjax, type: :feature
+  config.expect_with :rspec do |expectations|
+
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  SimpleCov.start("rails")
+end
